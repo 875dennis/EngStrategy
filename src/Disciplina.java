b9a686dc264a1753aa6f920c03eaea7ec1032e14
875dennis <86630541+875dennis@users.noreply.github.com>
2023-04-25 21:30:10 -0300

    Update Disciplina.java

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Disciplina {
+public class Disciplina {
+    private ICalcMedia _CalcMedia;
+    private double _Media;
+    private String _Situacao;
+    private String _Nome;
+    private double _P1;
+    private double _P2;
+    public String getNome()
+    {
+        return this._Nome;
+    }
+    public double getMedia()
+    {
+        return this._Media;
+    }
+    public String getSituacao()
+    {
+        return this._Situacao;
+    }
+    public void CalcularMedia()
+    {
+        var calc = this._CalcMedia.CalculaMedia(_P1,_P2);
+        this._Media = calc;
+        this._Situacao = this._CalcMedia.Situacao(calc);
+    }
+    public Disciplina()
+    {
+
+    }
+    public Disciplina(ICalcMedia calcMedia, String nome, double p1, double p2)
+    {
+        this._CalcMedia = calcMedia;
+        this._Nome = nome;
+        this._P1 = p1;
+        this._P2 =p2;
+    }
 }
