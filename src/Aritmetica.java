77399b2c21b60aa098555aa1b5209ebb32fcacef
875dennis <86630541+875dennis@users.noreply.github.com>
2023-04-25 21:29:54 -0300

    Update Aritmetica.java

--- src/Aritmetica.java
+++ src/Aritmetica.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Aritmetica {
+public class Aritmetica implements ICalcMedia{
+    @Override
+    public double CalculaMedia(double p1, double p2) {
+        return (p1+p2)/2;
+    }
+
+    @Override
+    public String Situacao(double resultado) {
+        return (resultado>5)?"Aprovado":"Reprovado";
+    }
 }
